72c7c10637bf47e3153ec4d7f0ff34aca159601d
mandeepjangra1996 <mj360502@gmail.com>
2023-07-07 06:29:39 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+//----------- Importing Modules -----------//
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+// db con
+
+mongoose.connect('mongodb://127.0.0.1:27017/habit-tracker',{useNewUrlParser:true})
+const db = mongoose.connection;
+
+db.on("error",()=>{console.log("error in conection");})
+db.once('open',()=>{console.log("Connected");})
+//-----EJS---------//
+app.use(expressLayouts);
+app.use("/assets", express.static('./assets'));
+app.set('view engine', 'ejs');
+
+//------BodyParser--------//
+app.use(express.urlencoded({ extended: false }));
+
+//---------Express Session----------//
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+//---------Connect Flash----------//
+app.use(flash());
+
+//---------Global Variables----------//
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+//-----Routes---------//
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port  ${PORT}`));
